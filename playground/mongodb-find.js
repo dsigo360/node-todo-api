243ce9c9bd0d633725562f01efe793f1f71bcc98
dsigo360 <dsigo360@me.com>
2018-07-01 20:07:42 -0500

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,52 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect(
+  'mongodb://localhost:27017/TodoApp',
+  (err, db) => {
+    if (err) {
+      return console.log('Unable to connect to MongoDB Server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos')
+    //   .find({
+    //     _id: new ObjectID('5b36942e362bf5ed4c695e35'),
+    //   })
+    //   .toArray()
+    //   .then(
+    //     docs => {
+    //       console.log('Todos');
+    //       console.log(JSON.stringify(docs, undefined, 2));
+    //     },
+    //     err => {
+    //       console.log(`Unable to fetch todos ${err}`);
+    //     },
+    //   );
+
+    // db.collection('Todos')
+    //   .find()
+    //   .count()
+    //   .then(
+    //     count => {
+    //       console.log(`Todos count: ${count}`);
+    //     },
+    //     err => {
+    //       console.log(`Unable to fetch todos ${err}`);
+    //     },
+    //   );
+
+    db.collection('Users')
+      .find({ name: 'Ducky' })
+      .toArray()
+      .then(
+        docs => {
+          console.log(JSON.stringify(docs, undefined, 2));
+        },
+        err => {
+          console.log(`Unable to fetch users ${err}`);
+        },
+      );
+    // db.close();
+  },
+);
